0b13111654327f2f2393e54567fb7fba102ae75d
rojiver.p <rojiver232@gmail.com>
2023-11-30 22:29:30 +0800

    added new features & improved ui

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
+let fonts = [
+    "Arial",
+    "Verdana",
+    "Tahoma",
+    "Trebuchet MS",
+    "Times New Roman",
+    "Georgia",
+    "Garamond",
+    "Courier New",
+    "Brush Script MT",
+    "Comic Sans MS"
+]
+
+function computer() {
+    let bruh = document.getElementById("fontFamily");
+
+    fonts.forEach(function(font) {
+        let x = document.createElement("option");
+        x.innerText = font;
+        bruh.appendChild(x);
+    })
+}
+
 function ok() {
     let a = String(document.getElementById("basta").value);
 
-    document.getElementById("result").innerText = a.split("\n\n").sort().join("\n\n");
+    document.getElementById("result").value = a.split("\n\n").sort().join("\n\n");
+}
+
+function notok() {
+    document.getElementById("result").style.fontSize = document.getElementById("fontSize").value + "px";
+    document.getElementById("result").style.fontFamily = String(document.getElementById("fontFamily").value);
 }
